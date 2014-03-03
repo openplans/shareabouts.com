92f62895bfc81dc9bb3c1ebb581ed691ed459172
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-03-03 10:37:06 -0500

    Comment out the api from the requirements, as it's included as a submodule

--- src/manage.py
+++ src/manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+REPO_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(os.path.join(REPO_DIR, 'lib', 'api', 'src'))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
