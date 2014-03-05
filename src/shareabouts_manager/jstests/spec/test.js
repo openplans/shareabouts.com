e1b2b42ccbc29296863012befce0fda63113786a
Aaron Ogle <atogle@gmail.com>
2014-03-05 18:25:13 -0500

    Add grunt

---
+++ src/shareabouts_manager/jstests/spec/test.js
@@ -0,0 +1,12 @@
+/*global describe it beforeEach Planbox assert sinon*/
+
+(function () {
+  'use strict';
+  describe('stub test', function () {
+
+    it('should run a test', function () {
+      assert.isTrue(true);
+    });
+
+  });
+}());
