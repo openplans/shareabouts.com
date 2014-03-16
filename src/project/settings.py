00e038031f9f089d8db071c12eb7a1d51d9af645
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-03-16 11:56:27 -0400

    Whitespace and formatting

--- src/project/settings.py
+++ src/project/settings.py
@@ -91,16 +91,15 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
-
-    "shareabouts_manager.context_processors.settings",
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'shareabouts_manager.context_processors.settings',
 )
 
 ROOT_URLCONF = 'project.urls'
