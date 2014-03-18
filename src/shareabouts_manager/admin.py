904a5e918623271075f62b2fbe1a2b9e162ab8b1
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-03-17 21:41:19 -0400

    Show error messages in the admin for multiple or no default account packages

--- src/shareabouts_manager/admin.py
+++ src/shareabouts_manager/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from django.utils.translation import ugettext as _
 from shareabouts_manager.models import UserProfile, AccountOverrides, AccountPackage, CreditCard
 
@@ -7,6 +8,37 @@ class AccountPackageAdmin (admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ('__str__', 'is_default')
     list_editable = ('is_default',)
+
+
+    def get_queryset(self, request):
+        qs = super(AccountPackageAdmin, self).get_queryset(request)
+
+        # If this is not a simple GET request, just return the base queryset
+        # immediately.
+        if request.method.lower() != 'get':
+            return qs
+
+        # Otherwise, calculate the number of packages with is_default and show
+        # a message if necessary
+        total_count = qs.count()
+        is_default_count = qs.filter(is_default=True).count()
+
+        if total_count == 0:
+            self.message_user(
+                request, "You must have at least one account package!",
+                level=messages.ERROR)
+        elif is_default_count == 0:
+            self.message_user(
+                request, "There is no default package selected! You must "
+                         "select a default package.",
+                level=messages.ERROR)
+        elif is_default_count > 1:
+            self.message_user(
+                request, "You have more than one package selected as default. "
+                         "Only one package should be default.",
+                level=messages.ERROR)
+
+        return qs
 
 
 class PackageOverridesInline (admin.StackedInline):
